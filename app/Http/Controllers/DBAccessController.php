e76beae3720a90be01b8feeffa56508c35c250f4
vagrant <vagrant@homestead>
2020-03-06 08:19:41 +0000

    code cleanup

--- app/Http/Controllers/DBAccessController.php
+++ app/Http/Controllers/DBAccessController.php
@@ -8,99 +8,6 @@ use DB;
 
 class DBAccessController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
-
-    public function db_connect_pdo($id)
-    {
-
-    $db = Database::findOrFail($id);
-    $dsn = $db->driver . ':host=' . $db->host . ';' .'dbname=' . $db->connection;
-    $dbuser = $db->username;
-    $dbpass = $db->password;
-    $connection = new PDO($dsn,$dbuser,$dbpass);
-
-    }
-
-    public function table_list_pdo($id, $connection)
-    {
-        $sql = 'SHOW TABLES';
-        $statement = $connection->prepare($sql)->execute();
-        $tables = $statement->fetchAll(PDO::FETCH_NUM);
-    }
 
     public function get_tables($db_name)
     {
@@ -113,16 +20,13 @@ class DBAccessController extends Controller
         $records = DB::connection($db_name)->table($table_name)->get();
         $columns =  DB::connection($db_name)->getSchemaBuilder()->getColumnListing($table_name);
         return view('databases.records_list', compact('table_name','db_name','columns','records'));
-
     }
 
     public function edit_records($db_name,$table_name,$record_id)
     {
-        $records = DB::connection($db_name)->table($table_name)->get()->toArray();
-        $columns =  DB::connection($db_name)->getSchemaBuilder()->getColumnListing($table_name);
+        $record = DB::connection($db_name)->table($table_name)->get()->where('id',$record_id);
         return view('databases.records_list', compact('table_name','db_name','columns','records'));
 
     }
-
 
 }
